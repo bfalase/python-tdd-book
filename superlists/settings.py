e2850f3ca13ba54fb2d3f18f7717c626d178811d
Bayo Falase <bfalase@gmail.com>
2020-08-30 18:40:29 -0700

    hack ALLOWED_HOSTS to be '*'

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'inyl==^=9fr10%5a=ud)^b^4rlj5d2^s0^+%l$%yzzc%pd9m3%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
